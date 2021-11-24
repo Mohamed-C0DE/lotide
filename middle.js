94c41f1dd1117b8e47a2239e4fe762c1c4179210
Mohamed Ali <12maali27@gmail.com>
2021-11-24 04:05:25 +0000

    function returning middle element(s) of an array

---
+++ middle.js
@@ -0,0 +1,49 @@
+// TEST/ASSERTION FUNCTIONS
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  const result = eqArrays(arr1, arr2);
+  if (result) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+// ACTUAL FUNCTION
+const middle = (arr) => {
+  const middleEl = [];
+  if (arr.length < 3) {
+    return middleEl;
+  } else if (arr.length % 2 === 0) {
+    let num = arr.length / 2;
+    middleEl.push(arr[num - 1], arr[num]);
+  } else {
+    let num = Math.floor(arr.length / 2);
+    middleEl.push(arr[num]);
+  }
+  return middleEl;
+};
+
+// TEST CODE
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
