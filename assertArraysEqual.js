641d17d7b104394f5c26db0faf7f2a1759ca334e
Mohamed Ali <12maali27@gmail.com>
2021-11-24 00:38:53 +0000

    file compares 2 arrays and console.logs an message

---
+++ assertArraysEqual.js
@@ -0,0 +1,17 @@
+const eqArrays = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  const result = eqArrays(arr1, arr2);
+  if (result) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
