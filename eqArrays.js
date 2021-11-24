6c7785a3880e3a55d091a5d29b916bb27050f010
Mohamed Ali <12maali27@gmail.com>
2021-11-24 00:26:34 +0000

    added new file (eqArrays.js) checks if two arrays are the same

---
+++ eqArrays.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+          return false;
+      }
+  }
+  return true;
+};
