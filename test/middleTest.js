21a790acf82dfcb4cb8095f69aea9d6f7024286d
Mohamed Ali <12maali27@gmail.com>
2021-11-29 17:31:38 +0000

    changed tailTest and middleTest to use mocha chai testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,20 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 const middle = require("../middle");
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe("#middle", () => {
+  it("should return middle value in odd array", () => {
+    const actual = middle([1, 2, 3]);
+    const expected = [2];
+    assert.deepEqual(actual, expected);
+  });
+  it("should return two values in even array", () => {
+    const actual = middle([1, 2, 3, 4]);
+    const expected = [2, 3];
+    assert.deepEqual(actual, expected);
+  });
+  it("should return empty array if array has 1 or 2 values", () => {
+    const actual = middle([1]);
+    const expected = [];
+    assert.deepEqual(actual, expected);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,10 @@
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const tail = require("../tail");
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+  it("should return array without the first index", () => {
+    const actual = tail(["Hello", "Lighthouse", "Labs"]);
+    const expected = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.deepEqual(actual, expected);
+  });
+});
